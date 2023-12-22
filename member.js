0145ab89dc23048ae94d5d42baad5d8a34218610
Yosua Wijaya <yosuawijaya089@gmail.com>
2023-12-22 20:19:38 +0700

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,30 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+
+    if (memberValue == "1") {
+        document.getElementById("memberSkills").style.display = "block";
+    } else {
+        document.getElementById("memberSkills").style.display = "none";
+    }
+}
